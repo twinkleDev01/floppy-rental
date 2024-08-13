1135e866a2ef093194fec1a978acec60c3d77d35
devasheeshresilience <devasheeshp@resiliencesoft.com>
2024-08-13 10:37:42 +0530

    ad and testimonial components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,21 +4,29 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/Header/header/header.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FooterComponent } from './components/footer/footer.component';
+import { AdPlacementComponent } from './components/ad-placement/ad-placement.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdPlacementComponent,
+    TestimonialComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    CarouselModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdPlacementComponent,
+    TestimonialComponent
   ]
 })
 export class SharedModule { }
