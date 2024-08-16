eaf366954fa895cf90eb9907c3ebf5b007620b26
Sandhya ojha <sandhyaojha@resiliencesoft.com>
2024-08-16 13:22:36 +0530

    feat:paginator

---
+++ src/app/shared/components/page-breadcrumb/page-breadcrumb.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
+import { routes } from '../../../app.routes';
+
+@Component({
+  selector: 'app-page-breadcrumb',
+  templateUrl: './page-breadcrumb.component.html',
+  styleUrl: './page-breadcrumb.component.scss'
+})
+export class PageBreadcrumbComponent {
+
+  public routes = routes;
+  base = '';
+  page = '';
+  last = '';
+  constructor(private srevice:SharedService) {
+    this.srevice.base.subscribe((res: string) => {
+      this.base = res?.replaceAll('-', ' ');
+    });
+    this.srevice.page.subscribe((res: string) => {
+      if (res === 'services') {
+        this.page = 'services';
+      } else if (res === 'AboutUs') {
+        this.page = 'AboutUs';
+      } else if (res === 'blog') {
+        this.page = 'blog';
+      } else {
+        this.last = this.page;
+        this.page = res?.replaceAll('-', ' ');
+      }
+    });
+    this.srevice.last.subscribe((res: string) => {
+      this.last = res?.replaceAll('-', ' ');
+    });
+  }
+}
+
+
