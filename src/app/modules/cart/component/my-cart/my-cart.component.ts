15204d28a916768e79dd6057bd6cf3f28d87cd26
Hemadrisahu1 <hemadarisahu@resiliencesoft.com>
2024-08-16 17:35:42 +0530

    added cart component

--- src/app/modules/cart/component/my-cart/my-cart.component.ts
+++ src/app/modules/cart/component/my-cart/my-cart.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CartService } from "../../../../shared/services/cart.service";
+
 
 @Component({
   selector: 'app-my-cart',
   templateUrl: './my-cart.component.html',
   styleUrl: './my-cart.component.scss'
 })
-export class MyCartComponent {
+export class MyCartComponent { 
+cartItems:any[]=[]
+OfferAndCoupon:any
+isUpdate:boolean = false
+  constructor(private cartService: CartService){
+    this.getCartItems()
+    this.getOfferAndCoupon()
+  }
+  
+  getCartItems(){
+    this.cartService.getAllCartItems().subscribe(data=> 
+    this.cartItems = data
+    )
+  }
 
+  getOfferAndCoupon(){
+    this.cartService.getOfferAndCoupon().subscribe((data:any)=>
+     this.OfferAndCoupon = data
+    )
+  }
+ 
+onInputChange(value: string, input: HTMLInputElement): void {
+  value = value.replace(/[^0-9]/g, '');
+  if (value) {
+    value = parseInt(value, 10).toString().padStart(2, '0');
+  } else {
+    return;
+  }
+  input.value = value;
+}
+// Handle blur to ensure formatting when input loses focus
+onBlur(cart: any): void {
+  cart.quantity = parseInt(cart.quantity, 10).toString().padStart(2, '0');
+}
+
+// Handle quantity change (+/- buttons)
+changeQuantity(cart: any, change: number): void {
+  let quantity = parseInt(cart.quantity, 10) || 0;
+  quantity += change;
+  if (quantity < 1) {
+    quantity = 1;
+  }
+  cart.quantity = quantity.toString().padStart(2, '0');
+}
+
+removeCartItems(item:any){
+  console.log(item.id)
+  this.cartItems=this.cartItems.filter(cart => cart.id != item.id)
+}
+
+updateCart(){
+  this.cartService.getUpdateCartDetails().subscribe(res=>{
+    if(res){
+      this.isUpdate = true;
+    }
+  }
+  )
+}
+
+proceedToCheckout(item:any){
+  console.log(item)
+  if(this.isUpdate){
+    alert('Are you sure you want to checkout this item')
+  }else{
+    alert('Please press Update cart button first before continuing proceed to checkout')
+  }
+}
 }
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,15 +1,45 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 
-const items = [ {
-  serviceImage:'',
-  title:'Niti Group Facility Services',
-  price:14000,
-  quantity:1,
-  discount :1,
-  tex:89,
-  currency:'',
-  }]
+export interface cartItemsType {
+  productTitle: string,
+  price: number,
+  currency: string,
+  quantity: number,
+  discount :number,
+  tex:number,
+}
+
+export interface couponCodesType{
+  offerType: string,
+  icon: string,
+  title: string,
+  sortDescription:string,
+  description: string,
+}
+
+const cartItems = [
+  {
+    id:0,
+    img:'https://static01.nyt.com/images/2023/07/10/well/00ASKWELL-EXPIRED-SKINCARE3/00ASKWELL-EXPIRED-SKINCARE3-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
+    productTitle: 'Niti Group Facility Services',
+    price: 14000,
+    currency: 'INR',
+    quantity: 1,
+    discount :1,
+    tex:89,
+  },
+  ];
+
+  const couponCodes = [
+    {
+      offerType: 'bank',
+      icon: '/images/bank_logo.png',
+      title: '20% off on Kotak Silk Cards',
+      sortDescription: '20% off up to INR 350',
+      description: 'SAVE INR 350 On This Order',
+    },
+  ];
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +49,15 @@ export class CartService {
   constructor() { }
 
   getAllCartItems(){
-    return of (items)
+    return of (cartItems)
+  }
+  
+  getOfferAndCoupon(){
+    return of (couponCodes)
+  }
+
+  getUpdateCartDetails(){
+    return of (cartItems)
   }
 
 }
