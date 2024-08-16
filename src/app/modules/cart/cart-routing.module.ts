6f4ef938fd4c5a5c1c84c551ba190db559f3ad52
Sandhya ojha <sandhyaojha@resiliencesoft.com>
2024-08-16 18:39:27 +0530

    feat:cart

--- src/app/modules/cart/cart-routing.module.ts
+++ src/app/modules/cart/cart-routing.module.ts
@@ -7,27 +7,7 @@ const routes: Routes = [
     path:'',
     component:MyCartComponent
   }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class CartRoutingModule { }
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CheckoutComponent } from './component/checkout/checkout.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: CheckoutComponent,
-  },
-  // {
-  //   path: '',
-  //   component: ,
-  // }
+ 
 ];
 
 @NgModule({
--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { MyCartComponent } from './component/my-cart/my-cart.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
+import { SharedModule } from '../../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    MyCartComponent
+    MyCartComponent,
+    CheckoutComponent
   ],
   imports: [
     CommonModule,
+    SharedModule,
     CartRoutingModule,
     FormsModule,
     ReactiveFormsModule,
