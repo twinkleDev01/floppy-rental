1475a464e65b6645ae6bb7b36dd5e2f0ba69444b
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-08-22 12:07:45 +0530

    about

--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutUSRoutingModule } from './about-us-routing.module';
+import { AboutUsComponent } from './about-us/about-us/about-us.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutUsComponent
+  ],
   imports: [
     CommonModule,
     AboutUSRoutingModule
