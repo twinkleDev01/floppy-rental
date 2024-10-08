7047ba16944e63b75984fff1bc6caec55a665c4b
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-10-08 11:24:58 +0530

    base url change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    ApiBaseUrl: 'https://firstfloppy.asptask.in/api/',
-    // ApiBaseUrl: 'https://cicd.asptask.in/api/',
+    // ApiBaseUrl: 'https://firstfloppy.asptask.in/api/',
+    ApiBaseUrl: 'https://cicd.asptask.in/api/',
 };
