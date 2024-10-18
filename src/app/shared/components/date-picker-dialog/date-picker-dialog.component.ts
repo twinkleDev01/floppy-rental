567e92e8138c3be29f80186b65dda25fce6d245f
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-10-18 13:34:46 +0530

    bug issue

--- src/app/shared/components/date-picker-dialog/date-picker-dialog.component.ts
+++ src/app/shared/components/date-picker-dialog/date-picker-dialog.component.ts
@@ -9,7 +9,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class DatePickerDialogComponent {
   selectedDay: string = 'Monday';
   daysOfWeek: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  officeHours = ['08:00 AM - 10:00 AM', '09:00 AM - 11:00 AM', '12:00 PM - 02:00 PM', '03:00 PM - 04:00 PM', '05:00 PM - 07:00'];
+  officeHours = ['08:00 AM - 10:00 AM', '09:00 AM - 11:00 AM', '12:00 PM - 02:00 PM', '03:00 PM - 04:00 PM', '05:00 PM - 07:00 PM'];
   currentDate:any
   selectedTime: string = '08:00 AM - 10:00 AM'
 
