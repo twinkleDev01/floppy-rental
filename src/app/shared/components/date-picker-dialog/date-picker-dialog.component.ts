e6f8bfca22f9607a2aeacd5cb98e192b42e3d1a9
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-10-24 16:52:31 +0530

    all bug points

--- src/app/shared/components/date-picker-dialog/date-picker-dialog.component.ts
+++ src/app/shared/components/date-picker-dialog/date-picker-dialog.component.ts
@@ -12,7 +12,7 @@ export class DatePickerDialogComponent {
   officeHours = ['08:00 AM - 10:00 AM', '09:00 AM - 11:00 AM', '12:00 PM - 02:00 PM', '03:00 PM - 04:00 PM', '05:00 PM - 07:00 PM'];
   currentDate:any
   selectedTime: string = '08:00 AM - 10:00 AM'
-
+  minDate: Date = new Date();
   constructor(public dialogRef: MatDialogRef<DatePickerDialogComponent>) {}
   ngOnInit(){
     console.log("Dialog Component")
