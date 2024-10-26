163b9a3870ea5312d6f18db56fc452029e0d7281
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-10-26 18:05:51 +0530

    home banner api response

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ export function app(): express.Express {
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
+    maxAge: '1d',
     index: 'index.html',
   }));
 
