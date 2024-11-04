f9ee1ca88f9246c03d49bdd4b52a273235fe0761
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-11-04 12:01:05 +0530

    add s on end point

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     // ApiBaseUrl: 'https://firstfloppy.asptask.in/api/',
     // ApiBaseUrl: 'https://cicd.asptask.in/api/',
-    ApiBaseUrl: 'http://webapi.firstfloppy.com/api/',
+    ApiBaseUrl: 'https://webapi.firstfloppy.com/api/',
 };
