bdd93375dcb1188bec1ea1bb773d67ee2cd63353
Rashmi2023 <122149402+Rashmi2023@users.noreply.github.com>
2024-11-25 15:00:57 +0530

    static page

---
+++ src/app/shared/components/anti-discrimination-policy/anti-discrimination-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anti-discrimination-policy',
+  templateUrl: './anti-discrimination-policy.component.html',
+  styleUrl: './anti-discrimination-policy.component.scss'
+})
+export class AntiDiscriminationPolicyComponent {
+
+}
